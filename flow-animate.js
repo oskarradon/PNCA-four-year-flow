13b6464f88c4588dc70e6cac058a51ffb6a6deab
temp <temp@ap00261.internal.pnca.edu>
2018-01-11 10:47:36 -0800

    use CSS vars (maybe have to change for IE later)

--- flow-animate.js
+++ flow-animate.js
@@ -1,25 +1,29 @@
+function animate() {
+  let tl = new TimelineLite();
 
-  function animate() {
-    let tl = new TimelineLite();
-    // circles
-    let blue = document.getElementById('flow__blue-circle');
+  // OBJECTS
 
-    let white = document.getElementById('flow__white-circle');
-    let black = document.getElementById('flow__black-circle');
-    // titles
-    let thesisTitle = document.getElementById('flow__title-thesis');
-    //other objects
-    let line = document.getElementById('flow__line');;
+  // circles
+  let blue = document.getElementById('flow__blue-circle');
+  let white = document.getElementById('flow__white-circle');
+  let black = document.getElementById('flow__black-circle');
 
-    tl.add( TweenLite.to(blue, 2, {left:-100}),
-            TweenLite.to(white, 2, {left:-200}),
-            TweenLite.to(black, 2, {left:-320}),
-            TweenLite.to(black, 2, {left:-310})
-           );
+  // titles
+  let thesisTitle = document.getElementById('flow__title-thesis');
 
-    tl.add( TweenLite.to(thesisTitle, 1, {opacity:1}),
-            TweenLite.to(line, 2, {left: -300, width: 1000})
-          );
-  }
+  //other objects
+  let line = document.getElementById('flow__line');
 
-  setTimeout( () => { animate() }, 1000 );
+
+  tl.add( TweenLite.to(blue, 2, {left:-100}),
+          TweenLite.to(white, 2, {left:-200}),
+          TweenLite.to(black, 2, {left:-320}),
+          TweenLite.to(black, 2, {left:-310})
+         );
+
+  tl.add( TweenLite.to(thesisTitle, 1, {opacity:1}),
+          TweenLite.to(line, 2, {left: -300, width: 1000})
+        );
+}
+
+setTimeout( () => { animate() }, 1000 );
