07dc095ff933b3698b77b1e6be50b5aa8b97f233
temp <temp@ap00261.internal.pnca.edu>
2018-01-11 10:55:34 -0800

    fix animations

--- flow-animate.js
+++ flow-animate.js
@@ -4,25 +4,24 @@ function animate() {
   // OBJECTS
 
   // circles
-  let blue = document.getElementById('flow__blue-circle');
-  let white = document.getElementById('flow__white-circle');
-  let black = document.getElementById('flow__black-circle');
+  let blue = document.getElementById( 'flow__blue-circle' );
+  let white = document.getElementById( 'flow__white-circle' );
+  let black = document.getElementById( 'flow__black-circle' );
 
   // titles
-  let thesisTitle = document.getElementById('flow__title-thesis');
+  let thesisTitle = document.getElementById( 'flow__title-thesis' );
 
-  //other objects
-  let line = document.getElementById('flow__line');
+  // other objects
+  let line = document.getElementById( 'flow__line' );
 
 
-  tl.add( TweenLite.to(blue, 2, {left:-100}),
-          TweenLite.to(white, 2, {left:-200}),
-          TweenLite.to(black, 2, {left:-320}),
-          TweenLite.to(black, 2, {left:-310})
+  tl.add( TweenLite.to( blue, 2, { left: 510 } ),
+          TweenLite.to( white, 2, { left: 820 } ),
+          TweenLite.to( black, 2, { left: 950 } ),
          );
 
-  tl.add( TweenLite.to(thesisTitle, 1, {opacity:1}),
-          TweenLite.to(line, 2, {left: -300, width: 1000})
+  tl.add( TweenLite.to( thesisTitle, 1, { opacity:1 } ),
+          TweenLite.to( line, 2, { left: -300, width: 1000 } )
         );
 }
 
